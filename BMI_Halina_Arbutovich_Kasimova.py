1ee078ded1a45d2f5c9ef285ae7ea6a4c0033964
Halina-Arbutovich-Kasimova <129683832+Halina-Arbutovich-Kasimova@users.noreply.github.com>
2023-04-09 20:51:50 +0300

    Update BMI_Halina_Arbutovich_Kasimova.py

--- BMI_Halina_Arbutovich_Kasimova.py
+++ BMI_Halina_Arbutovich_Kasimova.py
@@ -1,5 +1,5 @@
-height = float (input("Enter the height in cm "))
-weight =float( input("Enter the weight in kg "))
+height = float(input("Enter the height in cm "))
+weight = float(input("Enter the weight in kg "))
 BMI =(weight / ((height*0.01))**2)
 BMI2 = round(BMI)
 print ("Your body mass index = ",BMI2)
