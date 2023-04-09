88e5d6a99ddd6fc0cba197ede8b0a6708265eaef
Halina-Arbutovich-Kasimova <129683832+Halina-Arbutovich-Kasimova@users.noreply.github.com>
2023-04-09 20:52:35 +0300

    Update BMI_Halina_Arbutovich_Kasimova.py

--- BMI_Halina_Arbutovich_Kasimova.py
+++ BMI_Halina_Arbutovich_Kasimova.py
@@ -2,7 +2,7 @@ height = float(input("Enter the height in cm "))
 weight = float(input("Enter the weight in kg "))
 BMI =(weight / ((height*0.01))**2)
 BMI2 = round(BMI)
-print ("Your body mass index = ",BMI2)
+print("Your body mass index = ",BMI2)
 scale1 = 16
 scale2 = 40
 result=scale2-scale1-1
@@ -13,4 +13,4 @@ n = result
 d = l*n
 d[a]='/'
 d2=' '.join(d)
-print (scale1,d2,scale2)
+print(scale1,d2,scale2)
